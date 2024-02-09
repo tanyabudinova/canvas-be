4b0aaa9fc4359f08faa4b1b9001700abcfc81b93
Icaka <ico.ezekiev@gmail.com>
2024-02-09 19:57:00 +0200

    Pizza post

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -14,7 +14,11 @@ export class PostsService {
     p1.id = 1;
     p1.description = "Sunrise";
     p1.image = "https://www.surfertoday.com/images/stories/sunrise-sunset-facts.jpg"
-    return [p1];
+    const p2 = new Post();
+    p2.id = 2;
+    p2.description = 'Pizza';
+    p2.image = 'https://www.allrecipes.com/thmb/fFW1o307WSqFFYQ3-QXYVpnFj6E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/48727-Mikes-homemade-pizza-DDMFS-beauty-4x3-BG-2974-a7a9842c14e34ca699f3b7d7143256cf.jpg';
+    return [p1, p2];
   }
 
   findOne(id: number) {
