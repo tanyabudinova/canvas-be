f7247f18efd194dba26c4272c43cdeddc9e259b8
Icaka <ico.ezekiev@gmail.com>
2024-02-19 17:46:09 +0200

    db script

---
+++ src/database/data.sql
@@ -0,0 +1,9 @@
+--Using DB Browser to populate the db
+INSERT INTO Posts ('id', 'description', 'image', 'createdAt', 'updatedAt')
+VALUES (1, 'Sunrise', 'https://www.surfertoday.com/images/stories/sunrise-sunset-facts.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO Posts ('id', 'description', 'image', 'createdAt', 'updatedAt')
+VALUES (2, 'Pizza', 'https://www.allrecipes.com/thmb/fFW1o307WSqFFYQ3-QXYVpnFj6E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/48727-Mikes-homemade-pizza-DDMFS-beauty-4x3-BG-2974-a7a9842c14e34ca699f3b7d7143256cf.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO Posts ('id', 'description', 'image', 'createdAt', 'updatedAt')
+VALUES (3, 'Harold', 'https://upload.wikimedia.org/wikipedia/en/a/a4/Hide_the_Pain_Harold_%28Andr%C3%A1s_Arat%C3%B3%29.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO Posts ('id', 'description', 'image', 'createdAt', 'updatedAt')
+VALUES (4, 'Hmm', 'https://i.kym-cdn.com/entries/icons/original/000/000/106/Philoso.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
